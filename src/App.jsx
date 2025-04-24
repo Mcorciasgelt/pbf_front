08f7ea165a88105417de8b32d3e043810cabb4d4
Michael Corcias <michael.corcias@gelt.com>
2025-04-24 21:29:10 +0200

    inicialización

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+function App() {
+  return (
+    <div className="text-6xl font-bold text-blue-500 bg-yellow-200 p-10">
+      ¡Hola, Tu Familia Organizada!
+    </div>
+  );
+}
+
+export default App
