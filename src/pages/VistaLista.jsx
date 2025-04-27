4b4ff32a85deae849fc355ac08a385bc6a6d9045
Michael Corcias <michael.corcias@gelt.com>
2025-04-27 13:26:35 +0200

    vistas con filtro v2

--- src/pages/VistaLista.jsx
+++ src/pages/VistaLista.jsx
@@ -10,9 +10,11 @@ function VistaLista() {
   const { tareas, loading, error } = useTareas()
   const { miembros } = useMiembros()
 
+  const [vista, setVista] = useState('lista')
+
   const [filtroHijo, setFiltroHijo] = useState('todos')
   const [filtroPadre, setFiltroPadre] = useState('todos')
-  const [filtroEstado, setFiltroEstado] = useState('todos')
+  const [filtroEstado, setFiltroEstado] = useState('pendientes')
 
   const [tareasFiltradas, setTareasFiltradas] = useState([])
 
@@ -47,13 +49,27 @@ function VistaLista() {
     <div className="p-6">
       <h1 className="text-3xl font-bold mb-6 text-center">Lista de Tareas</h1>
 
+
+        {/* slector del tipo de vista que se verá en la pantalla */}
+        <div className="flex items-center justify-center gap-8 m-10">
+            <label className="font-bold text-2xl">Vistas:</label>
+            <select
+                value={vista}
+                onChange={(e) => setVista(e.target.value)}
+                className="border rounded px-4 py-2 text-2xl"
+            >
+                <option value="lista">Lista</option>
+                <option value="calendario">Calendario</option>
+            </select>
+        </div>
+
         {/* filtros. no lo entendí todo, pero funcionan */}
 
         <div className="flex flex-wrap justify-center gap-4 mb-6">
             <select
                 value={filtroHijo}
                 onChange={(e) => setFiltroHijo(e.target.value)}
-                className="border rounded px-4 py-2"
+                className="border rounded px-4 py-2 text-xl"
             >
                 <option value="todos">Todos los hijos</option>
                 {miembros
@@ -68,7 +84,7 @@ function VistaLista() {
             <select
                 value={filtroPadre}
                 onChange={(e) => setFiltroPadre(e.target.value)}
-                className="border rounded px-4 py-2"
+                className="border rounded px-4 py-2 text-xl"
             >
                 <option value="todos">Todos los padres</option>
                 {miembros
@@ -83,14 +99,29 @@ function VistaLista() {
             <select
                 value={filtroEstado}
                 onChange={(e) => setFiltroEstado(e.target.value)}
-                className="border rounded px-4 py-2"
+                className="border rounded px-4 py-2 text-xl"
             >
                 <option value="todas">Todas</option>
                 <option value="pendientes">Pendientes</option>
                 <option value="completadas">Completadas</option>
             </select>
-        </div>
 
+            <boton
+                onClick={() => {
+                    setFiltroPadre('todos')
+                    setFiltroHijo('todos')
+                    setFiltroEstado('pendientes')
+                }}
+                className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold text-xl py-2 px-4 rounded"
+                >
+                    Limpiar Filtros
+            </boton>
+
+        </div>
+    
+    {vista === 'lista' ? (
+    
+    <>
 
       {loading && <p className="text-gray-600 text-center">Cargando tareas...</p>}
       {error && <p className="text-red-500 text-center">{error}</p>}
@@ -103,23 +134,27 @@ function VistaLista() {
         <ul className="space-y-4">
           {tareasFiltradas.map((tarea) => (
             <li key={tarea._id} className="border rounded p-4 shadow">
-              <p className="font-bold">{tarea.titulo}</p>
-              <p className="text-sm text-gray-600">Fecha: {formatearFecha(tarea.fechaEntrega)}</p>
+              <p className="font-bold text-xl">{tarea.titulo}</p>
+              <p className="text-xl text-gray-600">Fecha: {formatearFecha(tarea.fechaEntrega)}</p>
               {user.tipo === 'padre' && (
-                <p className="text-sm text-gray-600">
+                <p className="text-l text-gray-600">
                   Hijo asociado: {tarea.hijosAsociados.map((h) => h.nombre).join(', ')}
                 </p>
               )}
-              <p className="text-sm text-gray-600">
+              <p className="text-l text-gray-600">
                 Padre responsable: {tarea.padreResponsable?.nombre || 'Sin asignar'}
               </p>
-              <p className="text-sm text-gray-600">
+              <p className="text-l text-gray-600">
                 Estado: {tarea.completada ? '✅ Completada' : '❌ Pendiente'}
               </p>
             </li>
           ))}
         </ul>
       )}
+      </>
+    ) : (
+        <p className='text-center text-gray-500'>Vista Calendario en construcción</p>
+    )}
     </div>
   );
 }
